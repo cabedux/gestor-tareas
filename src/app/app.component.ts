376e21488d062b6086d779390fe84223ddb3a795
scabedo <scabedo85@gmail.com>
2020-07-21 12:01:48 +0200

    add template task-list and array of Task in app-component and tesk-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gestor-tareas';
+  tasks: Task[] = [];
+
+  constructor(){
+    this.tasks.push({id: 1, description: 'prueba', complete: false});
+  }
 }
--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Task } from 'src/app/models/task';
 
 @Component({
   selector: 'app-task-list',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-list.component.scss']
 })
 export class TaskListComponent implements OnInit {
+  @Input() tasks: Task[] = [];
 
   constructor() { }
 
